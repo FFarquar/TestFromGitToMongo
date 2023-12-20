e1cb3d27ca586e5ba35d4dc5f7057acb08c67389
FFarquar <110162452+FFarquar@users.noreply.github.com>
2023-12-20 14:05:29 +1100

    Changed API Host address for Prod

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -10,9 +10,9 @@ namespace TestFromGitToMongo.Models
     //a class that exposes settings across the client and server
     public  class Settings : ISettings
     {
-        //public const  string API_BaseUrl = "https://graceful-deer-fedora.cyclic.app/";
+        public const  string API_BaseUrl = "https://graceful-deer-fedora.cyclic.app/";
         //public const string API_BaseUrl = "http://localhost:3000/";
-        public const string API_BaseUrl = "http://127.0.0.1:3000/";
+        //public const string API_BaseUrl = "http://127.0.0.1:3000/";
 
         //public static string jwttoken = string.Empty;
         public string jwttoken { get; set; }
