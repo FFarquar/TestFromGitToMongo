49c58d33fbe36cfaa959b8ca2de3771cdf65ee2d
FFarquar <110162452+FFarquar@users.noreply.github.com>
2023-12-25 07:54:06 +1100

    Change API address

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -10,8 +10,8 @@ namespace TestFromGitToMongo.Models
     //a class that exposes settings across the client and server
     public  class Settings : ISettings
     {
-        //public const  string API_BaseUrl = "https://graceful-deer-fedora.cyclic.app/api/";        //PROD LOCATION
-        public const string API_BaseUrl = "http://localhost:3000/api/";                                       //LOCAL Location
+        public const  string API_BaseUrl = "https://graceful-deer-fedora.cyclic.app/api/";        //PROD LOCATION
+        //public const string API_BaseUrl = "http://localhost:3000/api/";                                       //LOCAL Location
         
         
         //public const string API_BaseUrl = "http://127.0.0.1:3000/";
